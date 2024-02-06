5b9421a971aacbadad200017ac98bc6f572f1b08
haikhalroy <haikhalroy@gmail.com>
2024-02-06 03:58:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+require('dotenv').config()
+const { affinidiProvider } = require('@affinidi/passport-affinidi')
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = process.env.PORT || 3001;
+
+const initializeServer = async () => {
+
+    app.set('trust proxy', 1);
+    app.get('/', function (req, res, next) {
+        res.json({ success: 'Express' });
+    });
+
+    await affinidiProvider(app, {
+        id: "affinidi",
+        issuer: process.env.AFFINIDI_ISSUER,
+        client_id: process.env.AFFINIDI_CLIENT_ID,
+        client_secret: process.env.AFFINIDI_CLIENT_SECRET,
+        redirect_uris: ['http://localhost:3000/auth/callback']
+    });
+    
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
+
+}
+
+initializeServer();
